1a11849c5f0849147061b02be6c142f24821d0db
Ashutosh762-kumar <ashutosh762kumar@gmail.com>
2025-12-16 01:36:31 +0530

    Fix mongoose options for Node 22

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,7 @@ const connectDB = async () => {
     throw new Error("MONGO_URI is not defined in environment variables");
   }
 
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
